8cf4308214e20b4cf8e90662536159cbb5df346f
rebhuhnd <rebhuhnd@onid.orst.edu>
2014-06-18 15:18:23 -0700

    Progress on Visual Elements
    After studying Ross Hatton's Robot Drawing Tools package, I decided to
    try creating my own visual tools by combining robot links with 'Joints',
    which ideally can control the angles/elevation of each component
    separately. I may refactor the 'structs' into classes, since I believe
    it is easier to make links as handle objects. Although I am using URDF
    for ideas on how to structure the system, I am no longer planning to use
    URDF directly, as its structure is better suited for 3 dimensional
    representations.
    
    Successful implementation of matrix rotation for visual elements,
    although I need to work out how this works recursively with joints.
    thrown into the mix.
    
    After working out visual elements, links, and joints, I should be able
    to modify the display such that update functions will redraw the system
    in a way that makes sense.

---
+++ base/robot_drawing_tools_2/matrix_rotate.m
@@ -0,0 +1,24 @@
+function [Xnew,Ynew] = matrix_rotate(X,Y,theta,origin)
+% Rotates a set of patch coordinates and returns the results
+
+if length(origin) ~= 2
+    error('Origin must be a 2-0dimensional vector of x and y.');
+end
+
+%fprintf('X origin: %f, y Origin: %f\n',origin(1),origin(2));
+
+x = X'-origin(1);
+y = Y'-origin(1);
+
+sintheta = sin(-theta);
+costheta = cos(-theta);
+rot = [...
+    costheta sintheta;...
+    -sintheta costheta;...
+    ];
+result = rot*[x;y];
+Xnew = result(1,:)+origin(1);
+Ynew = result(2,:)+origin(2);
+
+end
+
