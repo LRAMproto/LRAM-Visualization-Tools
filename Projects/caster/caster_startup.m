d39300700712d968d1921fbb0095031e59558606
rebhuhnd <rebhuhnd@onid.orst.edu>
2014-06-30 16:47:49 -0700

    Documentation of Startup Routine
    Modified details on how the startup routine works.

--- Projects/caster/caster_startup.m
+++ Projects/caster/caster_startup.m
@@ -1,4 +1,6 @@
 function caster_startup
+% Starts the program, but does not keep track of the program handles. This
+% allows for users to start and shut down the program repeatedly.
     caster_startup_debug;
 end
 
--- Projects/caster/caster_startup_debug.m
+++ Projects/caster/caster_startup_debug.m
@@ -1,11 +1,14 @@
-function ph = viscore_startup
-%VISCORE_STARTUP Summary of this function goes here
-%   Detailed explanation goes here
+function ph = caster_startup_debug
+% This function allows for the user to recieve a copy of all program
+% handles that the startup routine generates.
 
+    % Generates all default settings afresh.
     make_caster_default_settings();
     
+    % Initializes a program core to manage update operations
     ph.core = VisualizerCore();
     
+    % Sets the settings file
     set(ph.core,...
         'settingsFile','caster_default_settings',...
         'debugMode',0);
@@ -13,7 +16,7 @@ function ph = viscore_startup
     % Loads stored settings into the core settings object.
     ph.core.LoadSettings();    
      
-   % Loads the Display
+   % Loads the Display and the Control panel.
    ph = connect_vis_display(ph);
    ph = connect_vis_control_panel(ph);
 
