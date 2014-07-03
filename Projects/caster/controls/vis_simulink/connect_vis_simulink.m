202a5d9f7dd03aef9bddc70d0ceaa4996e6ce835
rebhuhnd <rebhuhnd@onid.orst.edu>
2014-07-03 12:46:18 -0700

    Added startup message for simulink plugin.

--- Projects/caster/controls/vis_simulink/connect_vis_simulink.m
+++ Projects/caster/controls/vis_simulink/connect_vis_simulink.m
@@ -1,6 +1,9 @@
 function program_handles = connect_vis_simulink(ph)
 % Connects the visualizer display to the main program.
-open_system('casting_simulation');
+if ~bdIsLoaded('casting_simulation')
+    disp('Opening simulink file... this may several moments if running Simulink for the first time.');
+    open_system('casting_simulation');
+end
 ph.SimulinkTest = VisualizerPlugin('Simulink Input',ph.core);
 set(ph.SimulinkTest,'debugMode',0);
 ph.SimulinkTest.AddToPlugins();
