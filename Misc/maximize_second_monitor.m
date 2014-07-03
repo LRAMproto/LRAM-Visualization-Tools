feb449ce626a738a50ed81bc45af45b5a42a370b
rebhuhnd <rebhuhnd@onid.orst.edu>
2014-07-03 11:51:37 -0700

    Updates to Maximize Second Monitor Function
    Made updates to commenting and added meaningful error checking.

--- Misc/maximize_second_monitor.m
+++ Misc/maximize_second_monitor.m
@@ -1,13 +1,29 @@
 function position = maximize_second_monitor(fig)
+% Maximizes a display window in a second monitor, provided that the
+% monitors are aligned at the bottom edge of the screen.
+%
+% maximize_second_monitor(FH): attempts to move the figure with the handle
+% of FH to a second connected monitor.
+
+% Determines the boundries of the monitors on the screen
 p = get(0,'monitorpositions');
+% Checks to see if there is actually a second monitor connected.
+if isequal(size(p),[1,4])
+    error('Only one monitor detected. Please connect secnd monitor.');
+end
+% Calculates width and height.
 [width, height] = getwh(p);
-scroffset = 50;
-position = [p(2,1),p(1,2),width,height+scroffset]
+% Computes the new position to set the monitor at.
+position = [p(2,1),p(1,2),width,height];
+% Sets the position. If this is displaying incorrectly, try repositioning
+% the monitor in your OS, then restarting MATLAB.
 set(fig,'position',position);
 end
 
 function [width, height] = getwh(p)
-width = p(2,3)-p(2,1);
-height = p(2,4)-p(2,2);
+% Calculates width and height of a monitor from its minimum and maximum
+% edges.
+width = p(2,3)-p(2,1)+1;
+height = p(2,4)-p(2,2)+1;
 
     end
