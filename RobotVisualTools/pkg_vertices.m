48380a6714cb2bdab696cb271a3db2632a573a4b
rebhuhnd <rebhuhnd@onid.orst.edu>
2014-07-08 14:40:41 -0700

    Updated Vertex Packaging
    Minor edit to the function which produces a set of useable vertices to
    be more general in nature.

--- RobotVisualTools/pkg_vertices.m
+++ RobotVisualTools/pkg_vertices.m
@@ -1,7 +1,21 @@
-function vertices = pkg_vertices(x,y)
-
-    vertices.xdata = x;
-    vertices.ydata = y;
-
+function vertices = pkg_vertices(varargin)
+% Packages a set of vertices for ease of use
+    if ~isempty(varargin)
+        vertices.xdata = [];
+        vertices.ydata = [];
+        vertices.zdata = [];            
+    end
+    
+    if length(varargin) >= 2
+        vertices.xdata = varargin{1};
+        vertices.ydata = varargin{2};
+    end
+    
+    if length(varargin) >= 3
+    % May be defined in future versions of visualizer.
+        vertices.zdata = varargin{3};
+    else
+        vertices.zdata = [];            
+    end
+    
 end
-
