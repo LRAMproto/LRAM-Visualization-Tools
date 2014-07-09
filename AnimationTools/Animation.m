9b4b450bae8107d557590906aa5bb4f58223b304
rebhuhnd <rebhuhnd@onid.orst.edu>
2014-07-09 16:54:08 -0700

    Advanced Testing and Video Output
    Uses frame images to render a video. Not perfect just yet because MATLAB
    print is XORing the colors automatically.

--- AnimationTools/Animation.m
+++ AnimationTools/Animation.m
@@ -9,8 +9,10 @@ classdef Animation < hgsetget
         videoFile = 'untitled.avi';
         displayFigure;
         updateFcn;
-        frames = []
+        frames = {}
         imgCount = 0;
+        userData = [];
+        frameRate = 30;
     end
     
     methods
@@ -20,20 +22,35 @@ classdef Animation < hgsetget
             obj.displayFigure = displayFigure;
         end
         
+        function Reset(obj)
+            obj.frames = [];
+        end
+        
         function RenderFrame(obj)
             filename = sprintf('%s/%s%d.png',obj.saveDirectory,obj.name,obj.imgCount);
             print(obj.displayFigure,filename,'-dpng');            
             obj.imgCount = obj.imgCount + 1;
+            if ~isa(filename,'char')
+                error('frame filename not captured correctly.');
+            end
+            obj.frames{obj.imgCount} = filename;
         end
         
         function RunUpdateFcn(obj)
-            obj.updateFcn();
+            obj.updateFcn(obj,obj.imgCount+1);
         end
         
         function OutputToVideo(obj)
-            vidObj = VideoWriter([savedirectory,savefile]);
-            open(vidObj);
             
+            filepath = [obj.saveDirectory,'\',obj.videoFile];
+            vidObj = VideoWriter(filepath);
+            set(vidObj,'FrameRate',obj.frameRate);
+            open(vidObj);
+            for k = 1:length(obj.frames)
+                curFrame = imread(obj.frames{k});
+                writeVideo(vidObj,curFrame);
+            end
+            close(vidObj);
             
         end
         
