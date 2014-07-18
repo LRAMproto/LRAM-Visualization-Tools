cb6ca440c085547dffd09a39a6ac51989df2f9f8
caster <caster@LRAM-dev-1.(none)>
2014-07-18 15:39:35 -0700

    Added visualizer routine that launches the display only.

---
+++ Projects/caster/caster_startup_debug_display_only.m
@@ -0,0 +1,23 @@
+function ph = caster_startup_debug_display_only
+% This function allows for the user to recieve a copy of all program
+% handles that the startup routine generates.
+
+    % Generates all default settings afresh.
+    make_caster_default_settings();
+    
+    % Initializes a program core to manage update operations
+    ph.core = ProgramCore();
+    
+    % Sets the settings file
+    set(ph.core,...
+        'settingsFile','caster_default_settings',...
+        'debugMode',0);
+    
+    % Loads stored settings into the core settings object.
+    ph.core.LoadSettings();    
+     
+   % Loads the Display and the Control panel.
+   ph = connect_vis_display(ph);
+
+end
+
