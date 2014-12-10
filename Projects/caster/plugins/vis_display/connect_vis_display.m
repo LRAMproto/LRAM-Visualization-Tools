65275fec98acb1d0e466ed791e67d58671a6166c
David Rebhuhn <rebhuhnd@onid.oregonstate.edu>
2014-12-09 18:58:41 -0800

    Turned on debug mode for debugging purposes

--- Projects/caster/plugins/vis_display/connect_vis_display.m
+++ Projects/caster/plugins/vis_display/connect_vis_display.m
@@ -1,7 +1,7 @@
 function programHandles = connect_vis_display(ph)
 % Connects the visualizer display to the main program.
 ph.Plugins.Display = ProgramPlugin('Main Display Window',ph.core);
-set(ph.Plugins.Display,'debugMode',0);
+set(ph.Plugins.Display,'debugMode',1);
 set(ph.Plugins.Display,'guiFcn',@vis_display);
 ph.Plugins.Display.AddToPlugins();
 ph.Plugins.Display.LoadGui();
